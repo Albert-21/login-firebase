340e0d96362acc8affe5cd3271560c6459f55d6f
Albert-21 <alberto.ah14@hotmail.com>
2021-05-30 15:54:36 -0500

    Se agrego el modulo de notas para Mongo

--- src/Servicio/api.js
+++ src/Servicio/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_BASE = 'http://localhost:3001/api/'
+const URL_BASE = 'https://api-node-mongo.herokuapp.com/api/'
 const API = axios.create(
     {
         baseURL : URL_BASE,
